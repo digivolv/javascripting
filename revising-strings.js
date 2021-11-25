7b6019385a9a7f79bbdb681560b2801ef29e673a
digivolv <edwinjly@gmail.com>
2021-11-25 18:09:27 +0000

    Initial commit with all my work so far

---
+++ revising-strings.js
@@ -0,0 +1,9 @@
+//let example = 'this example exists'
+//example = example.replace('exists', 'is awesome')
+//console.log(example)
+
+let pizza = 'pizza is alright';
+
+pizza = pizza.replace('alright', 'wonderful');
+
+console.log(pizza)
